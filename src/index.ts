c392b0f8c0cbd639c1bbc7e6a77873510af7de07
JongBeom Kim <kimjbstar@gmail.com>
2021-03-28 22:01:28 +0900

    wip

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ async function main() {
     const villageForecast = new VillageForecast();
     villageForecast.setServiceKey("/eU/Y//rNBokLkGmOD9GyVv8SuuO9UjhA2rQUVXBmob5C9M3Jr7cKj5rHFVdrnL9yZ5dhuAOGHp5gVHUTkD7dA==")
     const result = await villageForecast.getUltraSrtNcst(37.4871167, 126.7274377);
+    // const result = await villageForecast.getUltraSrtFcst(37.4871167, 126.7274377);
+    // const result = await villageForecast.getVilageFcst(37.4871167, 126.7274377);
+    // const result = await villageForecast.getFcstVersion('ODAM');
+    console.dir(result, { depth: null })
 }
 main();
-
--- src/villageForecast.ts
+++ src/villageForecast.ts
@@ -1,37 +1,112 @@
 import axios from "axios";
 import * as querystring from 'querystring';
 import * as moment from 'moment';
-import * as fs from 'fs';
-import * as path from 'path';
 
-export enum WeatherCategory {
-    POP,
-    R06,
-    S06,
-    T3H,
-    TMN,
-    TMX,
-    WAV,
-    T1H,
-    RN1,
-    SKY,
-    UUU,
-    VVV,
-    REH,
-    PTY,
-    LGT,
-    VEC,
-    WSD,
+export const MapWeatherCategory = {
+    POP: '강수확률(%)',
+    R06: '6시간 강수량(mm)',
+    S06: '6시간 신적설(cm)',
+    T3H: '3시간 기온(c)',
+    TMN: '아침 최저기온(c)',
+    TMX: '낮 최고기온(c)',
+    WAV: '파고(M)',
+    T1H: '기온(c)',
+    RN1: '1시간 강수량(mm)',
+    SKY: '하늘상태',
+    UUU: '풍속(동서성분)',
+    VVV: '풍속(남북성분)',
+    REH: '습도(%)',
+    PTY: '강수형태',
+    LGT: '낙뢰',
+    VEC: '풍향(deg)',
+    WSD: '풍속(m/s)',
 }
 
-// SKY
+export const MapWeatherCodeSKY = {
+    '1': '맑음',
+    '2': '구름조금', // 삭제 (2019.06.4)
+    '3': '구름많음',
+    '4': '흐림'
+}
 
-// - 하늘상태(SKY) 코드 : 맑음(1), 구름많음(3), 흐림(4) 
-// * 구름조금(2) 삭제 (2019.06.4)
-// - 강수형태(PTY) 코드 : 없음(0), 비(1), 비/눈(2), 눈(3), 소나기(4), 빗방울(5), 빗방울/눈날림(6), 눈날림(7)
-// 여기서 비/눈은 비와 눈이 섞여 오는 것을 의미 (진눈개비)
-// * 빗방울(5), 빗방울/눈날림(6), 눈날림(7) 
+export const MapWeatherCodePTY = {
+    '0': '없음',
+    '1': '비',
+    '2': '비/눈',
+    '3': '눈',
+    '4': '소나기',
+    '5': '빗방울',
+    '6': '빗방울/눈날림',
+    '7': '눈날림'
+}
 
+export const MapWindDirection = [
+    'N-NE', 'NE-E', 'E-SE', 'SE-S', 'S-SW', 'SW-W', 'W-NW', 'NW-N'
+]
+
+export interface RawItem {
+    baseDate: string,
+    baseTime: string,
+    category: string,
+    nx: number,
+    ny: number,
+    obsrValue: string;
+    fcstDate?: string;
+    fcstTime?: string;
+    fcstValue?: string;
+}
+
+export interface RawBody {
+    dataType: 'JSON' | 'XML',
+    items: {
+        item: RawItem[]
+    },
+    pageNo: number,
+    numOfRows: number,
+    totalCount: number,
+}
+
+export interface RawHeader {
+    resultCode: string,
+    resultMsg: string
+}
+
+export interface RawResponse {
+    header: RawHeader,
+    body: RawBody
+}
+
+export interface WeatherItem {
+    category: string;
+    value: string;
+    desc: string,
+    valueDesc?: string;
+}
+
+export interface Location {
+    lat: number;
+    lng: number;
+}
+
+export interface WeatherGrid {
+    nx: number;
+    ny: number;
+}
+
+export interface NCastResult {
+    baseDate: string;
+    items: WeatherItem[]
+}
+
+export interface FCastResult {
+    baseDate: string;
+    forecastDate: FCastResultDate[]
+}
+
+export interface FCastResultDate {
+    date: string;
+    items: WeatherItem[],
+}
 
 export class VillageForecast {
     // LCC DFS 좌표변환을 위한 기초 자료
@@ -51,38 +126,94 @@ export class VillageForecast {
         this.serviceKey = src;
     }
 
-    // getUltraSrtNcst
-    // getUltraSrtFcst
-    // getVilageFcst
-    // getFcstVersion
-
-    async fetchPractice() {
-        const rawResponse = await fs.readFileSync(path.join(__dirname, '../examples/success01.json'));
-        const parsedResponse = JSON.parse(rawResponse.toString());
-        if (!parsedResponse || !parsedResponse['response']) {
-            console.log('something wrong. maybe service key')
-            return null;
-        }
-        const { header, body } = parsedResponse['response'];
+    parseRawResponse(rawResponse): RawResponse {
+        const res: RawResponse = rawResponse['response'];
+        const { header } = res;
         const { resultCode, resultMsg } = header;
         if (resultCode !== '00') {
             console.log(resultMsg);
             return null;
         }
-        const { dataType, items } = body;
-        console.log(body)
-        console.log(dataType);
-        console.log(items);
+        return res;
     }
 
-    async getUltraSrtNcst(lat: number, lng: number, datetime?, numOfRows = 100, pageNo = 1) {
+
+    parseItemRow(item: RawItem): WeatherItem {
+        const value = item.obsrValue ? item.obsrValue : item.fcstValue;
+        const row = {
+            category: item.category,
+            value: value,
+            desc: MapWeatherCategory[item.category],
+        }
+        if (item.category === 'PTY') {
+            row['valueDesc'] = MapWeatherCodePTY[value]
+        }
+        if (item.category === 'SKY') {
+            row['valueDesc'] = MapWeatherCodeSKY[value]
+        }
+        if (item.category === 'VEC') {
+            const vecValue = Number(value);
+            row['valueDesc'] = MapWindDirection[Math.floor((vecValue % 360) / 45)]
+        }
+        return row;
+    }
+
+
+
+    // 1. 초단기실황조회
+    async getUltraSrtNcst(lat: number, lng: number, datetime?, numOfRows = 1024, pageNo = 1): Promise<NCastResult> {
+        // 6시 발표(정시단위) -매시각 40분 이후 호출
         let baseDatetime: moment.Moment = null;
         if (!datetime) {
             baseDatetime = moment();
             if (baseDatetime.minutes() < 40) {
                 baseDatetime.subtract(1, 'hour')
             }
-            console.log(baseDatetime.format())
+        } else {
+            baseDatetime = moment(datetime);
+        }
+        const { nx, ny } = this.latLngToGrid(lat, lng)
+
+        const params = {
+            serviceKey: this.serviceKey,
+            pageNo: pageNo,
+            numOfRows: numOfRows,
+            dataType: 'JSON',
+            base_date: baseDatetime.format('YYYYMMDD'),
+            base_time: baseDatetime.format('HHmm'),
+            nx: nx,
+            ny: ny,
+        }
+        const qs = querystring.stringify(params);
+
+        const url = `${VillageForecast.baseURL}/getUltraSrtNcst?${qs}`;
+        console.log(url)
+        const rawResponse = await axios.get(url);
+        const { body } = this.parseRawResponse(rawResponse.data)
+
+        const result = body.items.item.reduce((result, item) => {
+            if (!result.baseDate) {
+                result.baseDate = moment(`${item.baseDate} ${item.baseTime}`).format('YYYY-MM-DD HH:mm')
+            }
+            const row = this.parseItemRow(item);
+            result.items.push(row)
+            return result;
+        }, {
+            baseDate: null,
+            items: [],
+        })
+
+        return result
+    }
+
+    // 2.초단기예보
+    async getUltraSrtFcst(lat: number, lng: number, datetime?, numOfRows = 1024, pageNo = 1): Promise<FCastResult> {
+        let baseDatetime: moment.Moment = null;
+        if (!datetime) {
+            baseDatetime = moment();
+            if (baseDatetime.minutes() < 45) {
+                baseDatetime.subtract(1, 'hour')
+            }
         } else {
             baseDatetime = moment(datetime);
         }
@@ -103,26 +234,116 @@ export class VillageForecast {
         const url = `${VillageForecast.baseURL}/getUltraSrtFcst?${qs}`;
         console.log(url)
 
-        const rawResponse = await fs.readFileSync(path.join(__dirname, '../examples/success01.json'));
-        const parsedResponse = JSON.parse(rawResponse.toString());
-        if (!parsedResponse || !parsedResponse['response']) {
-            console.log('something wrong. maybe service key')
-            return null;
-        }
-        const { header, body } = parsedResponse['response'];
-        const { resultCode, resultMsg } = header;
-        if (resultCode !== '00') {
-            console.log(resultMsg);
-            return null;
-        }
-        const { dataType, items } = body;
-        console.log(dataType);
-        console.log(items);
-        // const 
+        const rawResponse = await axios.get(url);
 
-        // TODO axios 
-        // if contains SERVICE_KEY_IS_NOT_REGISTERED_ERROR check
+        const { header, body } = this.parseRawResponse(rawResponse.data)
+        console.log(body.items.item)
 
+        const result = body.items.item.reduce((result, item) => {
+            if (!result.baseDate) {
+                result.baseDate = moment(`${item.baseDate} ${item.baseTime}`).format('YYYY-MM-DD HH:mm')
+            }
+            const key = item.fcstDate + item.fcstTime
+            if (!result.forecastDate[key]) {
+                result.forecastDate[key] = [];
+            }
+            const row = this.parseItemRow(item);
+            result.forecastDate[key].push(row)
+            return result;
+        }, {
+            baseDate: null,
+            forecastDate: [],
+        })
+
+        result.forecastDate = Object.entries(result.forecastDate).reduce((result, [k, v]) => {
+            const newRow = {
+                date: moment(k, 'YYYYMMDDHHmm').format('YYYY-MM-DD HH:mm'),
+                items: v,
+            }
+            result.push(newRow)
+            return result;
+        }, [])
+
+        return result
+    }
+
+    // 3. 동네예보조회
+    async getVilageFcst(lat: number, lng: number, datetime?, numOfRows = 1024, pageNo = 1): Promise<FCastResult> {
+        let baseDatetime: moment.Moment = null;
+        if (!datetime) {
+            baseDatetime = moment();
+            // TODO
+        } else {
+            baseDatetime = moment(datetime);
+        }
+
+        const { nx, ny } = this.latLngToGrid(lat, lng)
+
+        const params = {
+            serviceKey: this.serviceKey,
+            pageNo: pageNo,
+            numOfRows: numOfRows,
+            dataType: 'JSON',
+            base_date: baseDatetime.format('YYYYMMDD'),
+            base_time: baseDatetime.format('HHmm'),
+            nx: nx,
+            ny: ny,
+        }
+        const qs = querystring.stringify(params);
+
+        const url = `${VillageForecast.baseURL}/getVilageFcst?${qs}`;
+        console.log(url)
+
+        const rawResponse = await axios.get(url);
+
+        const { body } = this.parseRawResponse(rawResponse.data)
+
+        const result = body.items.item.reduce((result, item) => {
+            if (!result.baseDate) {
+                result.baseDate = moment(`${item.baseDate} ${item.baseTime}`).format('YYYY-MM-DD HH:mm')
+            }
+            const key = item.fcstDate + item.fcstTime
+            if (!result.forecastDate[key]) {
+                result.forecastDate[key] = [];
+            }
+            const row = this.parseItemRow(item);
+            result.forecastDate[key].push(row)
+            return result;
+        }, {
+            baseDate: null,
+            forecastDate: [],
+        })
+
+        result.forecastDate = Object.entries(result.forecastDate).reduce((result, [k, v]) => {
+            const newRow = {
+                date: moment(k, 'YYYYMMDDHHmm').format('YYYY-MM-DD HH:mm'),
+                items: v,
+            }
+            result.push(newRow)
+            return result;
+        }, [])
+
+        return result;
+
+    }
+
+    // 4. 동네예보조회
+    async getFcstVersion(type: 'ODAM' | 'VSRT' | 'SHRT', numOfRows = 1024, pageNo = 1) {
+        const params = {
+            serviceKey: this.serviceKey,
+            pageNo: pageNo,
+            numOfRows: numOfRows,
+            dataType: 'JSON',
+            ftype: type,
+            basedatetime: moment().format('YYYYMMDDHHmm')
+        }
+        const qs = querystring.stringify(params);
+
+        const url = `${VillageForecast.baseURL}/getFcstVersion?${qs}`;
+        console.log('url : ', url)
+
+        const rawResponse = await axios.get(url);
+        return rawResponse.data;
     }
 
     // LCC DFS 좌표변환 ( 위경도->좌표(grid) )
@@ -189,11 +410,11 @@ export class VillageForecast {
         let ro = tan(PI * 0.25 + olat * 0.5);
         ro = re * sf / pow(ro, sn);
 
-        var xn = nx - VillageForecast.XO;
-        var yn = ro - ny + VillageForecast.YO;
+        let xn = nx - VillageForecast.XO;
+        let yn = ro - ny + VillageForecast.YO;
         ra = sqrt(xn * xn + yn * yn);
         if (sn < 0.0) -ra;
-        var alat = pow((re * sf / ra), (1.0 / sn));
+        let alat = pow((re * sf / ra), (1.0 / sn));
         alat = 2.0 * atan(alat) - PI * 0.5;
 
         if (abs(xn) <= 0.0) {
@@ -207,24 +428,11 @@ export class VillageForecast {
             else
                 theta = atan2(xn, yn);
         }
-        var alon = theta / sn + olon;
+        let alon = theta / sn + olon;
         rs.lat = alat * RADDEG;
         rs.lng = alon * RADDEG;
         console.log(rs)
         return rs;
     }
-
 }
-
-export interface Location {
-    lat: number;
-    lng: number;
-}
-
-export interface WeatherGrid {
-    nx: number;
-    ny: number;
-}
-
-
 
